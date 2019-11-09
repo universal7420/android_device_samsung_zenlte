universal7420/android_device_samsung_zenltetmo

zenlte: Initialize Pie branch

AndroidProducts.mk
@@ -1,0 +1,2 @@
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/lineage_zenltetmo.mk

BoardConfig.mk
@@ -1,0 +1,39 @@
+# Copyright (C) 2015 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+#
+# This file sets variables that control the way modules are built
+# thorughout the system. It should not be used to conditionally
+# disable makefiles (the proper mechanism to control what gets
+# included in a build is to use PRODUCT_PACKAGES in a product
+# definition file).
+#
+
+# inherit from noblelte-common
+-include device/samsung/noblelte-common/BoardConfigCommon.mk
+
+# Inherit from proprietary vendor
+-include vendor/samsung/nobleltetmo/BoardConfigVendor.mk
+
+# Assert
+TARGET_OTA_ASSERT_DEVICE := zenltecan,zenltespr,zenltetmo
+
+# Include path
+TARGET_SPECIFIC_HEADER_PATH += device/samsung/zenltetmo/include
+
+# Kernel
+TARGET_KERNEL_CONFIG := lineageos_zenltetmo_defconfig
+
+# Radio
+BOARD_MODEM_TYPE := ss333

extract-files.sh
@@ -1,0 +1,24 @@
+#!/bin/bash
+#
+# Copyright (C) 2016 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+set -e
+
+export DEVICE=zenltetmo
+export DEVICE_COMMON=noblelte-common
+export VENDOR=samsung
+
+./../$DEVICE_COMMON/extract-files.sh $@

setup-makefiles.sh
@@ -1,0 +1,24 @@
+#!/bin/bash
+#
+# Copyright (C) 2016 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+set -e
+
+export DEVICE=zenltetmo
+export DEVICE_COMMON=noblelte-common
+export VENDOR=samsung
+
+./../$DEVICE_COMMON/setup-makefiles.sh $@

vendorsetup.sh
@@ -1,0 +1,1 @@
+add_lunch_combo lineage_zenltetmo-userdebug

universal7420/android_device_samsung_zenlte

AndroidProducts.mk
@@ -1,0 +1,2 @@
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/lineage_zenltejv.mk

BoardConfig.mk
@@ -1,0 +1,39 @@
+# Copyright (C) 2015 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+#
+# This file sets variables that control the way modules are built
+# thorughout the system. It should not be used to conditionally
+# disable makefiles (the proper mechanism to control what gets
+# included in a build is to use PRODUCT_PACKAGES in a product
+# definition file).
+#
+
+# inherit from noblelte-common
+-include device/samsung/noblelte-common/BoardConfigCommon.mk
+
+# Inherit from proprietary vendor
+-include vendor/samsung/nobleltejv/BoardConfigVendor.mk
+
+# Assert
+TARGET_OTA_ASSERT_DEVICE := zenlte,zenltedd,zenltedv,zenltejv,zenltektt,zenltelgt,zenlteskt,zenltezt,zenltexx
+
+# Include path
+TARGET_SPECIFIC_HEADER_PATH += device/samsung/zenltejv/include
+
+# Kernel
+TARGET_KERNEL_CONFIG := lineageos_zenltejv_defconfig
+
+# Radio
+BOARD_MODEM_TYPE := ss333

extract-files.sh
@@ -1,0 +1,24 @@
+#!/bin/bash
+#
+# Copyright (C) 2016 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+set -e
+
+export DEVICE=zenltejv
+export DEVICE_COMMON=noblelte-common
+export VENDOR=samsung
+
+./../$DEVICE_COMMON/extract-files.sh $@

setup-makefiles.sh
@@ -1,0 +1,24 @@
+#!/bin/bash
+#
+# Copyright (C) 2016 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+set -e
+
+export DEVICE=zenltejv
+export DEVICE_COMMON=noblelte-common
+export VENDOR=samsung
+
+./../$DEVICE_COMMON/setup-makefiles.sh $@

vendorsetup.sh
@@ -1,0 +1,1 @@
+add_lunch_combo lineage_zenltejv-userdebug
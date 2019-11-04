9fa50ddb76c77c373300a2d93bf771e44ba10ba7
ripee <ripee.dev@gmail.com>
2019-11-04 15:27:30 +0100

    zenlte: Initialize Pie branch

---
+++ device.mk
@@ -0,0 +1,30 @@
+#
+# Copyright (C) 2015 The Android Open Kang Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+LOCAL_PATH := device/samsung/zenltejv
+
+## device overlays
+DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
+
+# Vendor security patch level
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.lineage.build.vendor_security_patch=2018-08-01
+
+# Inherit from noblelte-common
+$(call inherit-product, device/samsung/noblelte-common/noblelte-common.mk)
+
+# Also get non-open-source specific aspects if available
+$(call inherit-product-if-exists, vendor/samsung/nobleltejv/nobleltejv-vendor.mk)
---
+++ lineage_zenltejv.mk
@@ -0,0 +1,47 @@
+# Copyright (C) 2013-2016 The CyanogenMod Project
+# Copyright (C) 2017-2019 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+#
+# This file is the build configuration for a full Android
+# build for zerofltecan hardware. This cleanly combines a set of
+# device-specific aspects (drivers) with a device-agnostic
+# product configuration (apps). Except for a few implementation
+# details, it only fundamentally contains two inherit-product
+# lines, full and zerofltecan, hence its name.
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from zenltejv device
+$(call inherit-product, device/samsung/zenltejv/device.mk)
+
+# Inherit some common LineageOS stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+# Set those variables here to overwrite the inherited values.
+PRODUCT_NAME := lineage_zenltejv
+PRODUCT_DEVICE := zenltejv
+PRODUCT_BRAND := samsung
+PRODUCT_MANUFACTURER := Samsung
+PRODUCT_MODEL := SM-G928F
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=zenltexx \
+    TARGET_DEVICE=zenlte \
+    PRIVATE_BUILD_DESC="zenltexx-user 7.0 NRD90M G928FXXU5CRI1 release-keys"
+
+BUILD_FINGERPRINT := "samsung/zenltexx/zenlte:7.0/NRD90M/G928FXXU5CRI1:user/release-keys"
