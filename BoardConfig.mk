e57c6b902e7254624af8466d11fe50230d8790d9
ripee <ripee.dev@gmail.com>
2019-11-16 01:32:09 +0100

    zenltejv: Set BTM_DEF_LOCAL_NAME

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -29,6 +29,9 @@
 # Assert
 TARGET_OTA_ASSERT_DEVICE := zenlte,zenltedd,zenltedv,zenltejv,zenltektt,zenltelgt,zenlteskt,zenltezt,zenltexx
 
+# Bluetooth
+BOARD_BLUETOOTH_OID_BUILDCFG_INCLUDE_DIR := device/samsung/zenltejv/bluetooth
+
 # Include path
 TARGET_SPECIFIC_HEADER_PATH += device/samsung/zenltejv/include
 
